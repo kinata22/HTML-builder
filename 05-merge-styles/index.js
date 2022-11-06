dfe365442b35e0af2c3c90064f52657cdcfe81a8
kinata <s@nata.cv.ua>
2022-11-06 21:56:13 +0100

    feat: task 5 - merge styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require("fs");
+const fsp = require("fs/promises");
+
+async function readableToString(readable) {
+  let result = '';
+  for await (const chunk of readable) {
+    result += chunk;
+  }
+  return result;
+}
+
+async function readStylesDirectory(stylesPath, bundlePath) {
+  let allFiles = [];
+  const files = await fsp.readdir(stylesPath, {withFileTypes: true});
+  for (let file of files) {
+    if(file.isFile() && path.extname(file.name) === '.css') {
+      allFiles.push(path.resolve(stylesPath, file.name));
+    }
+  }
+
+  let styles = [];
+  for await (let file of allFiles) {
+    const readableStream = fs.createReadStream(file, 'utf-8');
+    const text = await readableToString(readableStream);
+    styles.push(text);
+  }
+
+  var file = fs.createWriteStream(bundlePath);
+  file.on('error', function(err) { console.log(err) });
+  styles.forEach(value => file.write(`${value}`));
+  file.end();
+}
+
+const stylesDir = path.resolve(__dirname, 'styles');
+const distDir = path.resolve(__dirname, 'project-dist', 'bundle.css');
+
+readStylesDirectory(stylesDir, distDir);
+
